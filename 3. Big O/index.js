6aa6e5a2db8ac625978abbe32a1e531886b3ed1d
RRFayad <renan.r.fayad@gmail.com>
2024-12-02 09:05:42 -0300

    S02(Big O): 31. Big O and Scalability

--- 3. Big O/index.js
+++ 3. Big O/index.js
@@ -1,11 +1,15 @@
 const nemo = ["nemo"];
+const everyone = ["a", "b", "c", "d", "nemo", "f", "g", "h", "i", "j"];
 
 const findNemo = (arr) => {
+  let t0 = performance.now();
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === "nemo") {
       console.log("Found nemo!!");
     }
   }
+  let t1 = performance.now();
+  console.log(t1 - t0 + "miliseconds");
 };
 
-findNemo(nemo);
+findNemo(everyone);
